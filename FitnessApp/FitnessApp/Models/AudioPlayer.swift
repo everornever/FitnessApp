f084a051233de219390974394906f6be56d2260f
Leon <leon.account@gmx.net>
2022-12-09 15:47:15 +0100

    Fixed Audio playback

--- FitnessApp/FitnessApp/Models/AudioPlayer.swift
+++ FitnessApp/FitnessApp/Models/AudioPlayer.swift
@@ -11,8 +11,23 @@ class AudioPlayer {
     static var audioPlayer: AVAudioPlayer?
     
     static func playSound(soundFile: String) {
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try audioSession.setCategory(.playback, mode: .default, options: [.mixWithOthers])
+        } catch {
+            print("Failed to set audio session category.")
+        }
 
         if let path = Bundle.main.path(forResource: "\(soundFile).m4r", ofType: nil) {
+            
+            do {
+                try audioSession.setActive(true)
+            }  catch {
+                print("couldn't set session")
+            }
+            
             do {
                 audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
                 audioPlayer?.play()
